87bdf08e5f12c468f61bf002e1d61abb94a541da
tubassa283 <ynenghu@gmail.com>
2025-03-09 22:48:00 +0900

    修正

--- src/main.js
+++ src/main.js
@@ -3,14 +3,15 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 
-import HomePage from './components/HomePage.vue'; // 使用されているコンポーネントのみをインポート
+import LoginPage from './components/LoginPage.vue';
 import RegisterPage from './components/RegisterPage.vue';
+import HomePage from './components/HomePage.vue';
 import CreateRequirementPage from './components/CreateRequirementPage.vue';
 import RequirementDetailPage from './components/RequirementDetailPage.vue';
 
 const routes = [
-  { path: '/', component: HomePage }, // デフォルトのルートを変更
-  { path: '/register', component: RegisterPage },
+  { path: '/', component: LoginPage },
+  { path: '/register', component: RegisterPage }, // 新規登録ページのルートを追加
   { path: '/home', component: HomePage },
   { path: '/create-requirement', component: CreateRequirementPage },
   { path: '/requirement-detail/:id', component: RequirementDetailPage }
